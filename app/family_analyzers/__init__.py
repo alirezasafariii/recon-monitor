b242debafa52c33b2a1494c14754504193b9dc0a
Alireza Safari <88625680+alirezasafariii@users.noreply.github.com>
2026-08-12 17:53:20 +0330

    Export all dedicated family analyzers

--- app/family_analyzers/__init__.py
+++ app/family_analyzers/__init__.py
@@ -3,16 +3,23 @@ from .authentication_session import AuthenticationSessionFamilyAnalyzer
 from .base import FAMILY_ANALYZER_FRAMEWORK_VERSION, FamilyAnalyzer, FamilyAnalyzerContext
 from .bfla import BflaFamilyAnalyzer
 from .bola import BolaFamilyAnalyzer
+from .business_logic import BusinessLogicFamilyAnalyzer
+from .cors_misconfiguration import CorsMisconfigurationFamilyAnalyzer
 from .dom_xss import DomXssFamilyAnalyzer
 from .file_upload import FileUploadFamilyAnalyzer
+from .graphql_authorization import GraphqlAuthorizationFamilyAnalyzer
+from .graphql_data_exposure import GraphqlDataExposureFamilyAnalyzer
 from .information_disclosure import InformationDisclosureFamilyAnalyzer
 from .mass_assignment import MassAssignmentFamilyAnalyzer
 from .open_redirect import OpenRedirectFamilyAnalyzer
 from .path_traversal import PathTraversalFamilyAnalyzer
 from .postmessage_trust import PostMessageTrustFamilyAnalyzer
+from .race_condition import RaceConditionFamilyAnalyzer
 from .secret_exposure import SecretExposureFamilyAnalyzer
+from .sensitive_caching import SensitiveCachingFamilyAnalyzer
 from .source_map_exposure import SourceMapExposureFamilyAnalyzer
 from .ssrf import SsrfFamilyAnalyzer
+from .websocket_authorization import WebsocketAuthorizationFamilyAnalyzer
 from .router import analyzer_for_family, pending_families, registered_families, router_status
 
 __all__ = [
@@ -33,6 +40,13 @@ __all__ = [
     "InformationDisclosureFamilyAnalyzer",
     "SourceMapExposureFamilyAnalyzer",
     "SecretExposureFamilyAnalyzer",
+    "GraphqlAuthorizationFamilyAnalyzer",
+    "GraphqlDataExposureFamilyAnalyzer",
+    "BusinessLogicFamilyAnalyzer",
+    "RaceConditionFamilyAnalyzer",
+    "WebsocketAuthorizationFamilyAnalyzer",
+    "CorsMisconfigurationFamilyAnalyzer",
+    "SensitiveCachingFamilyAnalyzer",
     "analyzer_for_family",
     "registered_families",
     "pending_families",
